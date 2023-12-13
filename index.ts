58869d5a83ad7a7606e8b86abbfbb190998613c1
Henrik Hofmeister <henrik@newdawn.dk>
2023-12-13 13:05:44 +0100

    feat: Refactor to use plain express routes
    BREAKING CHANGES: API change adjusted for V2

--- index.ts
+++ index.ts
@@ -5,68 +5,34 @@
 
 import Path from 'path';
 
-import express, { Router } from 'express';
+import { RequestHandler } from 'express';
 import proxy from 'express-http-proxy';
-import Config, { ConfigProvider } from '@kapeta/sdk-config';
+import { ConfigProvider } from '@kapeta/sdk-config';
 
 const DEFAULT_SERVICE_TYPE = 'web';
 
-export class ProxyRoute {
-    private readonly _name: string;
-    private readonly _path: string;
-    private readonly _serviceType: string;
-    private readonly _router: Router;
-
-    private _targetUrl: string;
-    private _ready: boolean;
-
-    constructor(name: string, path: string, serviceType: string) {
-        this._name = name;
-        this._path = path;
-        this._serviceType = serviceType ?? DEFAULT_SERVICE_TYPE;
-        this._targetUrl = `http://${name.toLowerCase()}`;
-        this._ready = false;
-        this._router = express.Router();
-
-        Config.onReady(async (provider) => {
-            await this.init(provider);
-        });
+export const createProxyRoute = async (provider: ConfigProvider, name: string, serviceType?: string):Promise<RequestHandler> => {
+    let targetUrl = await provider.getServiceAddress(name, serviceType ?? DEFAULT_SERVICE_TYPE);
+    if (!targetUrl) {
+        throw new Error(`Service ${name} not found`);
     }
 
-    /**
-     * Called automatically during startup sequence.
-     *
-     * @param {ConfigProvider} provider
-     * @return {Promise<void>}
-     */
-    async init(provider: ConfigProvider) {
-        this._targetUrl = await provider.getServiceAddress(this._name, this._serviceType);
-        this._ready = true;
-
-        if (!this._targetUrl.endsWith('/')) {
-            this._targetUrl += '/';
-        }
-
-        const urlParts = new URL(this._targetUrl);
-
-        this._router.use(
-            this._path,
-            proxy(urlParts.host, {
-                https: urlParts.protocol === 'https',
-                proxyReqPathResolver: function (req) {
-                    const [path, query] = req.url.split('?');
-
-                    return Path.join(urlParts.pathname, path) + (query ? '?' + query : '');
-                },
-            })
-        );
-        console.log('Proxy route ready for %s. %s --> %s', this._name, this._path, this._targetUrl);
+    if (!targetUrl.endsWith('/')) {
+        targetUrl += '/';
     }
 
-    /**
-     * Returns expressjs Router object that contains proxy
-     */
-    toExpressRoute() {
-        return this._router;
-    }
+    const urlParts = new URL(targetUrl);
+
+    console.log('Proxy route ready for %s --> %s', name, targetUrl);
+
+    return proxy(urlParts.host, {
+        https: urlParts.protocol === 'https',
+        proxyReqPathResolver: function (req) {
+            const [path, query] = req.url.split('?');
+
+            return Path.join(urlParts.pathname, path) + (query ? '?' + query : '');
+        },
+    })
 }
+
+

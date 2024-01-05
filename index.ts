88685e2f8d906f85e8c9b081ad5ce0b6d25bf4fb
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-05 19:15:46 +0100

    feat: Adds decorator options to proxy route creator

--- index.ts
+++ index.ts
@@ -3,20 +3,35 @@
  * SPDX-License-Identifier: MIT
  */
 
+import type { RequestOptions } from 'node:http';
+import type { RequestHandler, Request } from 'express';
+import type { ConfigProvider } from '@kapeta/sdk-config';
+
 import Path from 'path';
-
-import { RequestHandler, Request } from 'express';
 import proxy from 'express-http-proxy';
-import { ConfigProvider } from '@kapeta/sdk-config';
 
-const DEFAULT_SERVICE_TYPE = 'web';
+
+const DEFAULT_SERVICE_TYPE:ProxyServiceType = 'web';
+
+export type ProxyServiceType = 'web' | 'rest' | 'http';
+export type ProxyRequestDecorator = (serviceName:string, serviceType:ProxyServiceType, proxyReqOpts: RequestOptions, srcReq:Request) => RequestOptions
+export type ProxyBodyDecorator = (serviceName:string, serviceType:ProxyServiceType, bodyContent: any, srcReq:Request) => any
+
+export type ProxyRouteOptions = {
+    // Apply decorators to the request options before sending each request
+    requestDecorators?: ProxyRequestDecorator[]
+
+    // Apply decorators to the body before sending each request
+    bodyDecorators?: ProxyBodyDecorator[]
+}
 
 export const createProxyRoute = async (
     provider: ConfigProvider,
     name: string,
-    serviceType?: string
+    serviceType: ProxyServiceType = DEFAULT_SERVICE_TYPE,
+    opts: ProxyRouteOptions = {}
 ): Promise<RequestHandler> => {
-    let targetUrl = await provider.getServiceAddress(name, serviceType ?? DEFAULT_SERVICE_TYPE);
+    let targetUrl = await provider.getServiceAddress(name, serviceType);
     if (!targetUrl) {
         throw new Error(`Service ${name} not found`);
     }
@@ -33,9 +48,23 @@ export const createProxyRoute = async (
         https: urlParts.protocol === 'https',
         timeout: 120000,
         limit: '1000mb',
+        proxyReqBodyDecorator: function (bodyContent, srcReq) {
+            if (opts?.bodyDecorators) {
+                opts.bodyDecorators.forEach((decorator) => {
+                    bodyContent = decorator(name, serviceType, bodyContent, srcReq);
+                });
+            }
+            return bodyContent;
+        },
         proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
             if (proxyReqOpts.headers) {
                 delete proxyReqOpts.headers['connection'];
+            }
+
+            if (opts?.requestDecorators) {
+                opts.requestDecorators.forEach((decorator) => {
+                    proxyReqOpts = decorator(name, serviceType, proxyReqOpts, srcReq);
+                });
             }
             return proxyReqOpts;
         },
